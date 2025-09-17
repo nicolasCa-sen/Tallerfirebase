4cc973960d44376938f28b066bfa3e97ee89f098
zCarreado <sergio.perez06@uptc.edu.co>
2025-09-16 21:54:06 -0500

    mantener la sesión usando firebaseAuthState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Login from "./components/login";
 import Notas from "./components/Notas";
+import { auth } from "./firebase";
 import { logout } from "./authUtils";
-
+import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // Listen for Firebase Auth state changes
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
 
   return (
     <div>
